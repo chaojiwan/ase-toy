0674d065cd24cc7bc3d4c69dd4269346de5fdf3a
Jin Liang <jin.liang2040@gmail.com>
2016-10-01 17:47:17 -0400

    Added React

--- ase-toy.py
+++ ase-toy.py
@@ -1,12 +1,13 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
---
+++ static/scripts/js/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by jinliang on 10/1/16.
+ */
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('content')
+);
