a8451014e7ab47621fe2137f850462dae50249ac
Jin Liang <jin.liang2040@gmail.com>
2016-10-02 09:14:00 -0500

    Created and validated sign up form using Flask-WTF

--- ase-toy.py
+++ ase-toy.py
@@ -1,12 +1,14 @@
-from flask import Flask
-from flask import render_template
-from models import db
+from flask import Flask, render_template, request
+from models import db, User
+from forms import SignupForm
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/toyflask'
 db.init_app(app)
+
+app.secret_key = "development-key"
 
 
 @app.route('/')
@@ -17,6 +19,23 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = SignupForm()
+
+    if request.method == "POST":
+        if not form.validate():
+            return render_template('signup.html', form=form)
+        else:
+            newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+            db.session.add(newuser)
+            db.session.commit()
+            return 'Success!'
+
+    elif request.method == "GET":
+        return render_template('signup.html', form=form)
 
 
 if __name__ == '__main__':
